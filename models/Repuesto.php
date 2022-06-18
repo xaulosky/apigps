53b943ccaac121c2dc17f46327882ee300b3f9cd
Patricio <patricio.poblete1701@alumnos.ubiobio.cl>
2022-06-17 20:57:08 -0400

    asdasd

--- models/Repuesto.php
+++ models/Repuesto.php
@@ -16,17 +16,15 @@ class Repuesto extends Conectar
     }
 
     /*Función para agregar un nuevo repuesto*/
-    function add_repuestos($nombreRepuesto, $cantidad, $fechaSolicitud, $fechaLlegada, $estadoRepuesto, $cTaller)
+    function add_repuestos($nombreRepuesto, $cantidad, $fechaSolicitud, $estadoRepuesto)
     {
         $conectar = parent::conexion();
-        $sql = "INSERT INTO repuesto (nombreRepuesto, cantidad, fechaSolicitud, fechaLlegada, estadoRepuesto, cTaller) VALUES (?, ?, ?, ?, ?, ?)";
+        $sql = "INSERT INTO repuesto (nombreRepuesto, cantidad, fechaSolicitud, estadoRepuesto) VALUES (?, ?, ?, ?)";
         $sql = $conectar->prepare($sql);
         $sql->bindValue(1, $nombreRepuesto);
         $sql->bindValue(2, $cantidad);
         $sql->bindValue(3, $fechaSolicitud);
-        $sql->bindValue(4, $fechaLlegada);
-        $sql->bindValue(5, $estadoRepuesto);
-        $sql->bindValue(6, $cTaller);
+        $sql->bindValue(4, $estadoRepuesto);
         $sql->execute();
         return $resultado = $sql->fetchAll(PDO::FETCH_ASSOC);
     }
