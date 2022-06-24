763768b62eb879636de50a6e83f038cccbcb85de
Patricio <patricio.poblete1701@alumnos.ubiobio.cl>
2022-06-23 20:25:06 -0400

    cosa repuesto

--- models/Repuesto.php
+++ models/Repuesto.php
@@ -6,25 +6,27 @@ class Repuesto extends Conectar
 {
 
     /*Función para obtener la lista de repuestos existentes en la BBDD*/
-    function get_repuestos()
+    function get_repuestos($cTaller)
     {
         $conectar = parent::conexion();
-        $sql = "SELECT * FROM repuesto";
+        $sql = "SELECT * FROM repuesto WHERE cTaller = ?";
         $sql = $conectar->prepare($sql);
+        $sql->bindValue(1, $cTaller);
         $sql->execute();
         return $resultado = $sql->fetchAll(PDO::FETCH_ASSOC);
     }
 
-    /*Función para agregar un nuevo repuesto*/
-    function add_repuestos($nombreRepuesto, $cantidad, $fechaSolicitud, $estadoRepuesto)
+/*Función para agregar un nuevo repuesto*/
+    public function add_repuestos($nombreRepuesto, $cantidad, $fechaSolicitud, $estadoRepuesto, $cTaller)
     {
         $conectar = parent::conexion();
-        $sql = "INSERT INTO repuesto (nombreRepuesto, cantidad, fechaSolicitud, estadoRepuesto) VALUES (?, ?, ?, ?)";
+        $sql = "INSERT INTO repuesto (nombreRepuesto, cantidad, fechaSolicitud, estadoRepuesto, cTaller) VALUES (?, ?, ?, ?, ?)";
         $sql = $conectar->prepare($sql);
         $sql->bindValue(1, $nombreRepuesto);
         $sql->bindValue(2, $cantidad);
         $sql->bindValue(3, $fechaSolicitud);
         $sql->bindValue(4, $estadoRepuesto);
+        $sql->bindValue(5, $cTaller);
         $sql->execute();
         return $resultado = $sql->fetchAll(PDO::FETCH_ASSOC);
     }
