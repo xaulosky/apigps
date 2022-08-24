5bd3ff002f5920fc27e7253da91ca42c4e175de0
nachogarcia1 <82839675+nachogarcia1@users.noreply.github.com>
2022-08-24 03:31:24 -0400

    ultimos cambios api

--- api/trabajo.php
+++ api/trabajo.php
@@ -20,15 +20,28 @@ switch ($method) {
         echo json_encode($trabajo->get_trabajos());
         break;
     case 'POST':
+        if (isset($body['nombreTrabajo']) && ($body['descripcionTrabajo']) && ($body['fechaEstimadaT']) && ($body['fechaRealT']) && ($body['estadoT']) && ($body['costoT']) && ($body['horasT']) && ($body['cOrdenTrabajo']) && ($body['cTipoT']) && ($body['cEmpleado'])) {
             $trabajo->crear_trabajo($body['nombreTrabajo'], $body['descripcionTrabajo'], $body['fechaEstimadaT'], $body['fechaRealT'], $body['estadoT'], $body['costoT'], $body['horasT'], $body['cOrdenTrabajo'], $body['cTipoT'], $body['cEmpleado'] );
         echo json_encode(array('msg' => 'Agregado Correctamente'));
+    } else {
+        echo json_encode(array('msg' => 'Faltan datos'));
+    }
         break;
     case 'PUT':
+        if (isset($body['cTrabajo']) && ($body['nombreTrabajo']) && ($body['descripcionTrabajo']) && ($body['fechaEstimadaT']) && ($body['fechaRealT']) && ($body['estadoT']) && ($body['costoT']) && ($body['horasT']) && ($body['cOrdenTrabajo']) && ($body['cTipoT']) && ($body['cEmpleado'])) {
         $trabajo->update_trabajo($body['cTrabajo'],$body['nombreTrabajo'], $body['descripcionTrabajo'], $body['fechaEstimadaT'], $body['fechaRealT'], $body['estadoT'], $body['costoT'], $body['horasT'], $body['cOrdenTrabajo'], $body['cTipoT'], $body['cEmpleado'] );
+        echo json_encode(array('msg' => 'Actualizado Correctamente'));
+    } else {
+        echo json_encode(array('msg' => 'Faltan datos o no existe el trabajo'));
+    }
         break;
     case 'DELETE':
+        if (isset($_GET['cTrabajo'])) {
         $trabajo->delete_trabajo($_GET['cTrabajo']);
         echo json_encode(array('msg' => 'Eliminado Correctamente'));
+    } else {
+        echo json_encode(array('msg' => 'Error, no se pudo eliminar el trabajo'));
+    }
         break;
 }
 
